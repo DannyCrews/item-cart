b3759743be25045e2724a014eb74442807742a0f
dannycrews <dancrews@me.com>
2016-12-21 12:46:29 -0500

    Add Item component and capability to edit item

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,23 +1,27 @@
 var AllItems = React.createClass({
-    handleDelete(id) {
-        this.props.handleDelete(id);
-    },
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
 
-    render() {
-        var items= this.props.items.map((item) => {
-            return (
-                <div key={item.id}>
-                    <h3>{item.name}</h3>
-                    <p>{item.description}</p>
-                    <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
-                </div>
-            )
-        });
+  onUpdate(item) {
+    this.props.onUpdate(item);
+  },
 
-        return(
-            <div>
-                {items}
-            </div>
+  render() {
+    var items= this.props.items.map((item) => {
+      return (
+        <div key={item.id}>
+        <Item item={item}
+          handleDelete={this.handleDelete.bind(this, item.id)}
+          handleUpdate={this.onUpdate}/>
+        </div>
         )
-    }
+    });
+
+    return(
+      <div>
+        {items}
+      </div>
+      )
+  }
 });
