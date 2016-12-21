79de1c3bafd83b35c87db8df7c7fb8dee40793f2
dannycrews <dancrews@me.com>
2016-12-21 11:55:27 -0500

    Add capability to delete items

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,28 +1,41 @@
 var Body = React.createClass({
-    getInitialState() {
-        return { items: [] }
-    },
+  getInitialState() {
+    return { items: [] }
+  },
 
+  componentDidMount() {
+    $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
+  },
 
-    componentDidMount() {
-        $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
-    },
+  handleSubmit(item) {
+    var newState = this.state.items.concat(item);
+    this.setState({ items: newState })
+  },
 
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/items/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeItemClient(id);
+      }
+    });
+  },
 
+  removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
 
-    handleSubmit(item) {
-        var newState = this.state.items.concat(item);
-        this.setState({ items: newState })
-    },
+    this.setState({ items: newItems });
+  },
 
-
-
-    render() {
-        return (
-            <div>
-                <NewItem handleSubmit={this.handleSubmit}/>
-                <AllItems  items={this.state.items} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+      <NewItem handleSubmit={this.handleSubmit}/>
+      <AllItems  items={this.state.items} handleDelete={this.handleDelete}/>
+      </div>
+      )
+  }
 });
